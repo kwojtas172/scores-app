4bcc04590c0f2f2f5c93205e4d6fa3be1e2f63b7
kwoy <gringo.first@gmail.com>
2020-05-27 17:25:23 +0200

    Updates

---
+++ precache-manifest.13db87e7555a12df73a132a1743c0865.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecaf62f37786297bb7bf3a98ae9739de",
+    "url": "/scores-app/index.html"
+  },
+  {
+    "revision": "fca74e109174e8e2b7cb",
+    "url": "/scores-app/static/css/main.894f7b19.chunk.css"
+  },
+  {
+    "revision": "012d10c52683859f897c",
+    "url": "/scores-app/static/js/2.a66c9ae7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/scores-app/static/js/2.a66c9ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fca74e109174e8e2b7cb",
+    "url": "/scores-app/static/js/main.38f99eac.chunk.js"
+  },
+  {
+    "revision": "7a1437e57c5468465c3e",
+    "url": "/scores-app/static/js/runtime-main.b8220089.js"
+  }
+]);
